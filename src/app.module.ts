e5bdecef17ac4400b47438a72836dcbce4aff51a
bruna freitas <bruna.freitas@nave.rs>
2021-06-18 10:19:56 -0300

    add

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Produto } from './produto.model';
+import { ProdutosController } from './produtos.controller';
+import { ProdutosService } from './produtos.service';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'produtos',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Produto]),
+  ],
+  controllers: [AppController, ProdutosController],
+  providers: [AppService, ProdutosService],
+})
+export class AppModule {}
